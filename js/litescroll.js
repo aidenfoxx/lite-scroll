bb15c27a298d2ff1b796e98b1bd51133591d202e
Aiden Foxx <aiden.foxx.mail@gmail.com>
2015-12-05 11:59:59 +0000

    Changed some binds from Window to Document

--- js/litescroll.js
+++ js/litescroll.js
@@ -126,11 +126,11 @@ LiteScroll.prototype.bindEvents = function()
     this.container.addEventListener('pointerdown', this._scrollStart.bind(this));
     this.container.addEventListener('MSPointerDown', this._scrollStart.bind(this));
 
-    window.addEventListener('mouseup', this._scrollEnd.bind(this));
-    window.addEventListener('touchend', this._scrollEnd.bind(this));
-    window.addEventListener('touchcancel', this._scrollEnd.bind(this));
-    window.addEventListener('pointerout', this._scrollEnd.bind(this));
-    window.addEventListener('MSPointerOut', this._scrollEnd.bind(this));
+    document.addEventListener('mouseup', this._scrollEnd.bind(this));
+    document.addEventListener('touchend', this._scrollEnd.bind(this));
+    document.addEventListener('touchcancel', this._scrollEnd.bind(this));
+    document.addEventListener('pointerout', this._scrollEnd.bind(this));
+    document.addEventListener('MSPointerOut', this._scrollEnd.bind(this));
     
     if (this.options.dynamicResize)
         window.addEventListener('resize', this.resize.bind(this));
